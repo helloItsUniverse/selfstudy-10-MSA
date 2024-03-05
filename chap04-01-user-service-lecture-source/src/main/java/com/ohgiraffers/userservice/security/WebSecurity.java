d9f284b8cfd2e657746091bbba1be8c92ba8d786
universe <hello.its.universe@gmail.com>
2024-03-05 12:23:39 +0900

    Spring Security를 활용한 로그인 성공 및 principle 객체까지 추출 완료

--- chap04-01-user-service-lecture-source/src/main/java/com/ohgiraffers/userservice/security/WebSecurity.java
+++ chap04-01-user-service-lecture-source/src/main/java/com/ohgiraffers/userservice/security/WebSecurity.java
@@ -1,31 +1,67 @@
 package com.ohgiraffers.userservice.security;
 
+import com.ohgiraffers.userservice.service.UserService;
+import jakarta.servlet.Filter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity {
-
     // 자동으로 로그인페이지로 redirect 하지 말고 우리 방식해도 하게 좀 허락해줘!
+
+    UserService userService;
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+    Environment env;
+
+    @Autowired
+    public WebSecurity(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder, Environment env) {
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.env = env;
+    }
 
     /* 설명. 인가(Authorization)용 메소드 (어디까지 허용할 것인가? 인가가 안돼있으면 403 에러) */
     @Bean       // bean 으로 하는 방식이 요즘 방식. extends 쓰면 옛날 방식
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
+        /* 설명. 로그인 시 추가할 내용 */
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
         /* 설명. JWT 로그인 처리를 할 것이므로 CSRF 는 신경 쓸 필요가 없다. */
         http.csrf((csrf) -> csrf.disable());
 
         http.authorizeHttpRequests((auth) -> auth
+//                .requestMatchers(new AntPathRequestMatcher("/login"))
+//                .permitAll()
                 .requestMatchers(new AntPathRequestMatcher("/users/**"))
-                .permitAll());
+                .permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/health_check"))
+                .permitAll()
+        )
+                .authenticationManager(authenticationManager);
+
+        http.addFilter(getAuthenticationFilter(authenticationManager));
 
         return http.build();
     }
 
     /* 설명. 인증(Authentication)용 메소드 (인증이 안돼있으면 401 에러) */
+    private Filter getAuthenticationFilter(AuthenticationManager authenticationManager) {
+        return new AuthenticationFilter(authenticationManager, userService, env);
+    }
+
 }
--- chap04-01-user-service-lecture-source/src/main/java/com/ohgiraffers/userservice/service/UserServiceImpl.java
+++ chap04-01-user-service-lecture-source/src/main/java/com/ohgiraffers/userservice/service/UserServiceImpl.java
@@ -6,9 +6,13 @@ import com.ohgiraffers.userservice.repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 @Service
@@ -42,5 +46,26 @@ public class UserServiceImpl implements UserService{
         userEntity.setEncryptedPwd(bCryptPasswordEncoder.encode(userDTO.getPwd()));
 
         userRepository.save(userEntity);
+    }
+
+    /* 설명. UserDetailsService 인터페이스 상속 이후
+     *  DB에서 로그인 사용자 정보 조회 후 UserDetails 타입으로 반환하는 기능 구현
+    * */
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        // 일단 조회해온다.
+        UserEntity userEntity = userRepository.findByEmail(email);
+
+        if (userEntity == null) {
+            throw new UsernameNotFoundException(email + "아이디의 유저는 존재하지 않음");
+        }
+
+        return new User(userEntity.getEmail(), userEntity.getEncryptedPwd(),
+                true, true, true, true,
+                new ArrayList<>());
+
+        // UserDetails 타입으로 맞춰서 반환해주기만 하면
+        // 그 다음 이어질 raw/encrypted pwd 대조 작업은 내부적으로 해준다.
     }
 }
